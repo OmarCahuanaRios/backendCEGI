cab870f14a0be8841b5f2e7d79ddf26c2c80cb43
Moises Molina <w7moises@gmail.com>
2024-02-23 13:57:08 -0500

    audited entity added

--- src/main/java/com/backend/app/BackendCegiApplication.java
+++ src/main/java/com/backend/app/BackendCegiApplication.java
@@ -1,10 +1,28 @@
 package com.backend.app;
 
+import com.backend.app.config.audit.SpringSecurityAuditorAware;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class BackendCegiApplication {
+
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return new SpringSecurityAuditorAware();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendCegiApplication.class, args);
