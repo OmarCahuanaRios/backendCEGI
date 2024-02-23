1d4634bc13828ae014f44439fb65b3dc1df44de7
Moises Molina <w7moises@gmail.com>
2024-02-23 17:06:08 -0500

    visitant entity added

---
+++ src/main/java/com/backend/app/model/Visitant.java
@@ -0,0 +1,36 @@
+package com.backend.app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.envers.AuditTable;
+import org.hibernate.envers.Audited;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "visitants")
+@Audited
+@AuditTable(value = "aud_visitant")
+public class Visitant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "document_id", nullable = false, unique = true)
+    private String documentId;
+
+
+}
