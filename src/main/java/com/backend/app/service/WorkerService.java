672e89165423a0c9c9720625e6f2ec3f3988a94a
Moises Molina <w7moises@gmail.com>
2024-02-23 14:47:34 -0500

    worker service added

---
+++ src/main/java/com/backend/app/service/WorkerService.java
@@ -0,0 +1,20 @@
+package com.backend.app.service;
+
+import com.backend.app.dto.WorkerDto;
+import com.backend.app.dto.create.WorkerCreateDto;
+
+import java.util.List;
+
+public interface WorkerService {
+
+    List<WorkerDto> findAllWorkers();
+
+    WorkerDto findWorkerById(Integer id);
+
+    WorkerDto createWorker(WorkerCreateDto workerCreateDto);
+
+    WorkerDto updateWorker(Integer id, WorkerCreateDto workerCreateDto);
+
+    WorkerDto deleteWorker(Integer id);
+
+}
