2ed8d06902cb25fbe447c8642d29b8be267fd448
Moises Molina <w7moises@gmail.com>
2024-02-23 12:35:26 -0500

    authentication dto added

---
+++ src/main/java/com/backend/app/dto/CreateUserDto.java
@@ -0,0 +1,29 @@
+package com.backend.app.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateUserDto {
+
+    @NotEmpty(message = "firstname is required")
+    private String firstName;
+
+    @NotEmpty(message = "lastname is required")
+    private String lastName;
+
+    @NotEmpty(message = "email is required")
+    @Email
+    private String email;
+
+    @NotEmpty(message = "password is required")
+    private String password;
+}
