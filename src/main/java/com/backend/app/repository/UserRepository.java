a1342cc12493aad987547d36690f68554a91dec4
Moises Molina <w7moises@gmail.com>
2024-02-23 12:37:48 -0500

    authentication repository added

---
+++ src/main/java/com/backend/app/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.backend.app.repository;
+
+import com.backend.app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
