ab97d3ce5847bfe42b57391fa8182eef9c902bef
Moises Molina <w7moises@gmail.com>
2024-02-24 18:49:09 -0500

    visit repository added

---
+++ src/main/java/com/backend/app/repository/VisitRepository.java
@@ -0,0 +1,9 @@
+package com.backend.app.repository;
+
+import com.backend.app.model.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitRepository extends JpaRepository<Visit, Integer> {
+}
